37d9f77d2e8f1314cc754544fcbe9fb176572223
XennaExtra <kamiljarmolowicz9@gmail.com>
2024-11-28 20:50:28 +0100

    Initial algorithm finished. Primitive data representation added.

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import numpy as np
+from aco import ACO
+
 
 
 def readData(citiesFilePath):
@@ -30,7 +33,54 @@ def trimData(citiesIn):
 
     return citiesOut
 
-def computePaths(citiesIn):
-    # magic stuff
+def getPoints(citiesIn):
+    points = []
+    for i in range(len(citiesIn)):
+        point = [citiesIn[i]['x'], citiesIn[i]['y'], citiesIn[i]['cityName']]
+        points.append(point)
+    return points
 
-    return []
+def getDistanceMatrix(points):
+    n = len(points)
+    distanceMatrix = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            distanceMatrix[i,j] = np.sqrt((points[i][0] - points[j][0])**2 + (points[i][1] - points[j][1])**2)
+    np.fill_diagonal(distanceMatrix, np.inf)
+    return distanceMatrix
+
+
+def computePaths(citiesIn):
+    points = getPoints(citiesIn)  # Get [x, y, cityName]
+    distanceMatrix = getDistanceMatrix(points)
+
+    # Run ACO algorithm
+    aco = ACO(distanceMatrix, points, 10, 5, 100, 0.95, 1, 1)  # Adjusted parameters
+    shortest = aco.run()
+
+    shortest_indices = shortest[0]  # Indices of cities in the shortest path
+    shortest_distance = shortest[1]  # Total distance of the shortest path
+
+    # Prepare data structure to hold path and distances
+    path_data = {"points": [], "distances": []}
+
+    # Populate points and distances
+    for (a, b) in shortest_indices:
+        # Add full point info: (x, y, cityName)
+        if points[a] not in path_data["points"]:
+            path_data["points"].append(points[a])  # Full point info for plotting
+        path_data["points"].append(points[b])
+
+        # Add distance
+        distance = distanceMatrix[a, b]
+        path_data["distances"].append(float(distance))
+
+    # Print results
+    print("Path points (coordinates and names):", path_data["points"])
+    print("Distances between points:", path_data["distances"])
+    print(f"Total distance: {shortest_distance:.2f}")
+
+    return path_data, shortest_distance
+
+
+
--- main.py
+++ main.py
@@ -11,7 +11,16 @@ if __name__ == '__main__':
     # display window with info - c 
 
     # compute path - k
-    paths = computePaths(cities);
+    pathData, totalDistance = computePaths(cities)
+
+    print(pathData) #[points:[x1,y1,name1],[x2,y2,name2]..., distances: [distance1-2, distance2-3,...]
+    # To access points use pathData["points"]
+    print(pathData["points"])
+    # To access point distances use pathData["distances"]
+    print(pathData["distances"])
+    print(totalDistance)
+
+
 
     # display summary - c
 
