79b855c4cb2a4daad03bdac7f4ecf0045c0203ee
Cezary Androsiuk PC <czandros@wp.pl>
2024-11-28 01:56:23 +0100

    implemented function 'readData' and some minor changes

--- main.py
+++ main.py
@@ -1,64 +1,29 @@
 
-# data 1
-nodes = [
-    {'id': 1, 'name': 'city 1'}, 
-    {'id': 2, 'name': 'city 2'}, 
-    {'id': 3, 'name': 'city 3'}, 
-    {'id': 4, 'name': 'city 4'}
-]
-connections = [
-    {
-        'node 1': 1,
-        'node 2': 2,
-        'weight': 0.2
-    },
-    {
-        'node 1': 2,
-        'node 2': 3,
-        'weight': 0.4
-    },
-    {
-        'node 1': 3,
-        'node 2': 4,
-        'weight': 0.8
-    }
-]
 
-# data 2
-nodes = [
-    {
-        'id': 1,
-        'name': 'city 1',
-        'connections': [
-            {'node': 2, 'weight': 0.2}
-        ]
-    }, 
-    {
-        'id': 2,
-        'name': 'city 2',
-        'connections': [
-            {'node': 3, 'weight': 0.4},
-            {'node': 1, 'weight': 0.2}
-        ]
-    }, 
-    {
-        'id': 3,
-        'name': 'city 3',
-        'connections': [
-            {'node': 4, 'weight': 0.8},
-            {'node': 2, 'weight': 0.4}
-        ]
-    }, 
-    {
-        'id': 4,
-        'name': 'city 4',
-        'connections': [
-            {'node': 3, 'weight': 0.8}
-        ]
-    }
-]
+def readData(citiesFilePath):
+    cities = []
+    try:
+        with open(citiesFilePath, 'r', encoding='utf-8') as citiesFile:
+            # print("oppened")
+            i = 0
+            for rawCity in citiesFile:
+                # print(f"index {i}")
+                values = rawCity.strip().split(' ')
+
+                cityName = values[2].replace('_', ' ')
+
+                cities.append(dict(
+                    index=i, 
+                    x=int(values[0]), y=int(values[1]), 
+                    cityName=cityName, 
+                    population=values[3]
+                ))
+                i+=1
+    except Exception as e:
+        print(f"Cannot open {citiesFilePath}: {e}")
+
+    return cities
 
 if __name__ == '__main__':
-    print('HelloWorld("print")')
-
-    
+    cities = readData("city.data.txt")
+    print(cities)
