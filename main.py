3b3a43f2d45848f2c838e7813133e9ab6e19cd6e
XennaExtra <kamiljarmolowicz9@gmail.com>
2024-11-28 20:53:41 +0100

    Minor changes in main to improve readability

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ if __name__ == '__main__':
 
     print(pathData) #[points:[x1,y1,name1],[x2,y2,name2]..., distances: [distance1-2, distance2-3,...]
     # To access points use pathData["points"]
+    print("\n\n\n")
     print(pathData["points"])
     # To access point distances use pathData["distances"]
+    print("\n\n\n")
     print(pathData["distances"])
-    print(totalDistance)
+    print("\n\n\n")
+    print(f"Total distance: {totalDistance}")
 
 
 
